ab3dddee26faff54bbe9646dac939a5e6495721a
Alyson Felipe <alysonforever@gmail.com>
2024-05-23 19:32:10 -0300

    fix: secrete lake update mapview-sample1.js
    Remover key de api do ESRI

--- public/javascripts/mapview-sample1.js
+++ public/javascripts/mapview-sample1.js
@@ -1,5 +1,5 @@
 require(["esri/config","esri/Map", "esri/views/MapView"], (esriConfig, Map, MapView) => {
-	esriConfig.apiKey = "AAPK7f721bf6c36d4afbac47832be33b2f7aWIAcxoX0bM5fbknH9D_ansC_RLlo1fbEm9xO50mIPCnXIJeEgCfxq1uEsbKuPVVH"
+	esriConfig.apiKey = "<MY_KEY>"
   const map = new Map({
 		basemap: "arcgis-topographic",
 	});
